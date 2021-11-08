4050bf5a525e6b248773315ce0e36c576a2a43a5
Forrest Rabideau <forrest.rabideau@gmail.com>
2021-11-08 11:18:11 -0800

    1st commit

---
+++ OperationsExercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperationsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vars with hardcoded values for a and b
+            int a = 17;
+            int b = 4;
+
+            // vars toe store results from different math operation
+            int intSum = a + b;
+            int intDiff = a - b;
+            int intProduct = a * b;
+            int intQuotient = a / b;
+            int intRemainder = a % b;
+
+
+            // prints out results to CMD window
+            Console.WriteLine($"17+4 is {intSum}");
+            Console.WriteLine($"17-4 is {intDiff}");
+            Console.WriteLine($"17*4 is {intProduct}");
+            Console.WriteLine($"17/4 is {intQuotient} remainder {intRemainder}");
+        }
+    }
+}
